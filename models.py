d174f20b0ef21388d91e81806cfa099440018f7a
jabloco <zefir4eg@gmail.com>
2021-11-29 21:15:41 +0300

    add new models in models.py

--- models.py
+++ models.py
@@ -2,6 +2,13 @@ from sqlalchemy import Column, Integer, String
 
 from db import Base, engine
 
+
+# связующая таблица  для м2м
+user_product = pass
+
+def get_or_create():
+    pass
+
 class product(Base):
     __tablename__ = "products"
     id = Column(Integer, primary_key=True)
@@ -10,6 +17,14 @@ class product(Base):
 
     def __repr__(self):
         return f'Product {self.id} {self.description}'
+
+
+class price(Base):
+    pass
+
+
+class user(Base):
+    pass
 
 
 if __name__ == '__main__':
