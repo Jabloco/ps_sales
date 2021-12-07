dfedca40ca4f65af48266efc7dc4700d819da002
jabloco <zefir4eg@gmail.com>
2021-12-07 21:36:47 +0300

    логика пайплайна перенесена в db_worker

--- collect_data.py
+++ collect_data.py
@@ -2,8 +2,9 @@ from random import randint
 from time import sleep
 
 
-from html_parser import get_html, get_sales, get_max_pages, get_product_details, get_products, get_html_selenium
+from html_parser import get_html, get_max_pages, get_product_details, get_products, get_html_selenium
 from constants import BASE_URL, URL_REGION, URL_CATEGORY_ALL
+from models import Product, Price
 
 
 def max_page_in_category(url: str) -> int:
@@ -34,7 +35,29 @@ def products_url_on_page(url: str) -> list:
     return
 
 
-if __name__ == '__main__':
+def all_url_in_category(category_url:str) -> list:
+    """
+    Функция для получения всех ссылок в категории
+
+    Принимаетсылку на категорию
+
+    Возвращает список ссылок
+    """
+    all_products_in_category = []
+    
+    max_page = max_page_in_category(category_url)
+    if max_page:
+        for page in range(1, max_page + 1):
+            page_url = f'{category_url}{str(page)}'
+            sleep(randint(1, 5))
+            products_on_page = products_url_on_page(page_url)
+            if products_on_page:
+                all_products_in_category.extend(products_on_page)
+        return all_products_in_category
+    return
+
+
+def db_worker():
     """
     Пайплайн в общем виде:
         заходим на страницу категории
@@ -45,19 +68,18 @@ if __name__ == '__main__':
     """
     category_all_url = BASE_URL + URL_REGION + URL_CATEGORY_ALL
 
-    max_page = max_page_in_category(category_all_url)
-    if max_page:
-        all_products_in_category = []
-        for page in range(1, max_page + 1):
-            page_url = f'{category_all_url}{str(page)}'
-            sleep(randint(1, 5))
-            products_on_page = products_url_on_page(page_url)
-            if products_on_page:
-                all_products_in_category.extend(products_on_page)
+    url_in_category = all_url_in_category(category_all_url)
 
-    if all_products_in_category:
-        for product in all_products_in_category:
-            product_url = f'{BASE_URL}{product}'
+    if url_in_category:
+        for url_product in url_in_category:
+            product_url = f'{BASE_URL}{url_product}'
             sleep(randint(1, 5))
             product_detail = get_product_details(get_html(product_url))
             print(product_detail)
+            # parsed_title = product_detail['title']
+            # parsed_description = product_detail['description']
+            # parsed_url = url_product
+
+
+if __name__ == '__main__':
+    db_worker()
