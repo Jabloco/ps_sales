2d679fc3a715297342de03254be10c7c3c7b4047
jabloco <zefir4eg@gmail.com>
2021-12-16 22:01:31 +0300

    2 more tests

--- test_collect_data.py
+++ test_collect_data.py
@@ -9,7 +9,7 @@ def test_write_to_db_product_not_in_db():
         'is_ps_plus_price': False,
         'description': 'Text'
     }
-    assert write_to_db(product_detail, None) == (False, 'Price final 1000, price original None')
+    assert write_to_db(product_detail, None) == (False, f"Price final {product_detail['price_final']}, price original {product_detail['price_original']}")
 
 
 def test_write_to_db_produtct_in_db():
@@ -22,3 +22,36 @@ def test_write_to_db_produtct_in_db():
         'description': 'Text'
     }
     assert write_to_db(product_detail, None) == None
+
+def test_write_to_db_price_update():
+    product_detail = {
+        'title': 'Name1',
+        'price_final': '2000',
+        'price_original': None,
+        'url': '/ru/name1',
+        'is_ps_plus_price': False,
+        'description': 'Text'
+    }
+    assert write_to_db(product_detail, None) == (True, f"Price final {product_detail['price_final']}, price original {product_detail['price_original']}")
+
+def test_write_to_db_price_update_2():
+    product_detail = {
+        'title': 'Name1',
+        'price_final': '2000',
+        'price_original': None,
+        'url': '/ru/name1',
+        'is_ps_plus_price': False,
+        'description': 'Text'
+    }
+    assert write_to_db(product_detail, None) == (True, f"Price final {product_detail['price_final']}, price original {product_detail['price_original']}")
+
+def test_write_to_db_price_update_3():
+    product_detail = {
+        'title': 'Name1',
+        'price_final': '3000',
+        'price_original': None,
+        'url': '/ru/name1',
+        'is_ps_plus_price': False,
+        'description': 'Text'
+    }
+    assert write_to_db(product_detail, None) == (True, f"Price final {product_detail['price_final']}, price original {product_detail['price_original']}")
